61357977aedfe61a7b1588261e0eeb432173b4b2
Himanshu Singla <singlahim0003@gmail.com>
2024-05-22 13:07:11 +0530

    first commit

---
+++ firstproject/src/firstproject/firstproject.java
@@ -0,0 +1,10 @@
+package firstproject;
+
+public class firstproject {
+
+	public static void main(String args[])
+	{
+		System.out.println("Hi himanshu , How are you! i know you are a star and rich man in the world");
+		
+	}
+}
