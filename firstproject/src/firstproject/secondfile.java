531f2000937c50a4c6d7afff6abcf3f95b48cc95
Himanshu Singla <singlahim0003@gmail.com>
2024-05-22 15:32:09 +0530

    My second file with program : Sum

---
+++ firstproject/src/firstproject/secondfile.java
@@ -0,0 +1,20 @@
+package firstproject;
+
+public class secondfile {
+	
+	 
+	public static void main(String args[])
+	{
+	
+		int a = 10;
+		int b =55;
+			
+		 int c= a+b;
+		
+	System.out.println("Sum is "+ c );
+	
+		
+		
+	}
+
+}
